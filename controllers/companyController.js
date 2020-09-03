423d05b96fbf789bb87211f184196c4218592534
Tanzeel ur Rehman <tanzeel.dw@gmail.com>
2020-09-04 03:51:13 +0500

    Company update methods added

--- controllers/companyController.js
+++ controllers/companyController.js
@@ -1,6 +1,7 @@
 const company = require('../models/company');
 const Company = require('../models/company');
 const System = require('../models/system');
+const Countries = require('../models/countries');
 
 const async = require('async');
 const { body, validationResult } = require('express-validator'); 
@@ -16,22 +17,14 @@ exports.showAllCompanies = (req, res, next) => {
 };
 
 exports.showCompany = (req, res, next) => {
-  async.parallel({
-    company: callback => {Company.findById(req.params.id).exec(callback)},
-    companySystems: callback => {System.find({'company': req.params.id}, 'name')}
-  }, (err, results) => {
-    if (err) return next(err);
-    if (results.company === null) {
-      const err = new Error('Company Not Found');
-      err.status = 404;
-      return next(err);
-    }
-    res.render('showCompany', {title: 'Manufacturer Page', company: results.company, systems: results.companySystems});
-  });
+  Company.findById(req.params.id)
+    .exec((err, company) => {
+      res.render('showCompany', {title: company.name, company});
+    });
 };
 
 exports.createCompanyGET = (req, res, next) => {// let companies = {};
-  res.render('companyForm', {title: 'Add Company'});
+  res.render('companyForm', {title: 'Add Company', countries: Countries.countries});
 };
 
 exports.createCompanyPOST = [
@@ -67,12 +60,45 @@ exports.createCompanyPOST = [
 ];
 
 exports.updateCompanyGET = (req, res, next) => {
-  res.send('PENDING');
+  Company.findById(req.params.id)
+    .exec((err, company) => {
+      if (err) return next(err);
+      res.render('companyForm', {title: 'Update Company Data', company, countries: Countries.countries});
+    });
 };
 
-exports.updateCompanyPOST = (req, res, next) => {
-  res.send('PENDING');
-};
+exports.updateCompanyPOST = [
+  body('name')
+    .not().isEmpty()
+    .isLength({min: 1})
+    .trim()
+    .escape(),
+  body('country')
+    .isLength({min: 1})
+    .trim()
+    .escape(),
+  body('founded')
+    .isNumeric({min: 1972, max: new Date().getFullYear()}),
+  
+  (req, res, next) => {
+    const errors = validationResult(req);
+    const company = new Company({
+      _id: req.params.id,
+      name: req.body.name,
+      founded: req.body.founded,
+      country: req.body.country
+    });
+
+    if (!errors.isEmpty()) {
+      res.render('companyForm', {title: 'Update Company Data', company, countries: Countries.countries, errors: errors.array()});
+    } else {
+      Company.findByIdAndUpdate(req.params.id, company, {}, (err, thisCompany) => {
+        if (err) return next(err);
+        res.redirect(thisCompany.url);
+      });
+    }
+  }
+];
 
 exports.deleteCompanyGET = (req, res, next) => {
   res.send('PENDING');
